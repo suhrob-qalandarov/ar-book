5551b698dad4667f36d5ec89810409bc3eabcc59
athimjon <tonym5one@gmail.com>
2025-06-18 16:40:12 +0500

    Initial commit

---
+++ src/main/java/org/example/arbook/ArBookApplication.java
@@ -0,0 +1,13 @@
+package org.example.arbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/arbook/ArBookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.arbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
