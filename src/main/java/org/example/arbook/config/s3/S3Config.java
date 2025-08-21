35a1619045f967b91de5e90625a735d16bc7b0fd
Suhrob <suhrob_kalandarov@outlook.com>
2025-08-21 11:25:27 +0500

    Added S3 endpoint configuration and enabled path-style access in `S3Config`.

--- src/main/java/org/example/arbook/config/s3/S3Config.java
+++ src/main/java/org/example/arbook/config/s3/S3Config.java
@@ -9,9 +9,11 @@ import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
 
 @Configuration
-//@Profile("prod")
 public class S3Config {
     @Value("${aws.s3.region}")
     private String region;
@@ -22,13 +24,22 @@ public class S3Config {
     @Value("${aws.s3.secret-access-key}")
     private String secretAccessKey;
 
+    @Value("${aws.s3.endpoint}")
+    private String endpoint;
 
     @Bean
     public S3Client s3Client() {
         AwsCredentials credentials = AwsBasicCredentials.create(accessKey, secretAccessKey);
+
         return S3Client.builder()
                 .region(Region.of(region))
+                .endpointOverride(URI.create(endpoint))
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .serviceConfiguration(
+                        S3Configuration.builder()
+                                .pathStyleAccessEnabled(true)
+                                .build()
+                )
                 .build();
     }
 }
